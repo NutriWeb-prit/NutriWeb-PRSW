127a5f98c03eb211bc13d37db8a17fd61628a4a3
NutriWeb <nutriweb.prit@gmail.com>
2024-08-05 16:30:06 -0300

    Add files via upload

---
+++ app/routes/router.js
@@ -0,0 +1,40 @@
+var express = require("express");
+var router = express.Router();
+
+
+router.get("/", function (req, res) {
+    res.render('pages/indexHome')
+});
+
+router.get("/login", function (req, res) {
+    res.render('pages/indexLogin')
+});
+
+router.get("/cadastro", function (req, res) {
+    res.render('pages/indexCadastrar')
+});
+
+router.get("/cadastronutri", function (req, res) {
+    res.render('pages/indexCadastrarNutri')
+});
+
+router.get("/cadastrocliente", function (req, res) {
+    res.render('pages/indexCadastroCliente')
+});
+
+router.get("/entrar", function (req, res) {
+    res.render('pages/indexEntrar')
+});
+
+router.get("/perfilnutri", function (req, res) {
+    res.render('pages/indexPerfilNutri')
+});
+
+router.get("/profissionais", function (req, res) {
+    res.render('pages/indexProfissionais')
+});
+router.get("/ranking", function (req, res) {
+    res.render('pages/indexRanking')
+});
+
+module.exports = router;
