b44b66c1def026e0bd02dfdd43dc54b7dcb3ecc5
felpsss-leudb <feesouza101@gmail.com>
2024-08-20 14:25:32 -0300

    ajustes de codigos

--- app/public/js/indexMenu.js
+++ app/public/js/indexMenu.js
@@ -1,8 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     const menuIcon = document.querySelector('.menu-icon');
     const mobileMenu = document.querySelector('.mobile-menu');
+    const menuClose = document.querySelector('.menu-close');
 
     menuIcon.addEventListener('click', function() {
         mobileMenu.classList.toggle('hidden');
+        document.menuIcon.style.display = "none";
+        menuClose.classList.toggle('active');
     });
 });
