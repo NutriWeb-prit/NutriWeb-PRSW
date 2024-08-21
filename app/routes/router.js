69145816d0e3bdf1aa7a2b8344138584fd58e993
Felipe Silva <feesouza101@gmail.com>
2024-08-21 10:22:15 -0300

    validação de formulários

--- app/routes/router.js
+++ app/routes/router.js
@@ -1,25 +1,82 @@
 var express = require("express");
 var router = express.Router();
+const { body, validationResult } = require("express-validator");
+
+router.get("/login", function (req, res) {
+    res.render('pages/indexLogin', { retorno: null, valores: {email:"", senha:""}, listaErros: null});
+});
+
+router.post(
+    "/entrar", 
+    
+    body("email").isEmail().withMessage("Insira um Email válido!"),
+    body("senha").isLength({min:5}).withMessage("A senha deve conter 5 ou mais caracteres!"),
+    
+    function (req, res) {
+        const listaErros = validationResult(req);
+        if (listaErros.isEmpty()) {
+            return res.render("pages/indexLogin", {retorno: null, valores: {email: req.body.email, senha: req.body.senha}, listaErros:null});
+        }else {
+            console.log(listaErros);
+            return res.render("pages/indexLogin", {retorno: null, valores: {email: req.body.email, senha: req.body.senha}, listaErros:listaErros});
+        }
+    }
+);
+
+router.get("/cadastrocliente", function (req, res) {
+    res.render('pages/indexCadastroCliente', { retorno: null, valores: {nome:"", email:"", senha:""}, listaErros: null});
+});
+
+router.post(
+    "/cadastrarcliente", 
+    
+    body("nome").isLength({min:2}).withMessage("O nome deve conter 2 ou mais caracteres!"),
+    body("email").isEmail().withMessage("Insira um Email válido!"),
+    body("senha").isLength({min:5}).withMessage("A senha deve conter 5 ou mais caracteres!"),
+    
+    function (req, res) {
+        const listaErros = validationResult(req);
+        if (listaErros.isEmpty()) {
+            return res.render("pages/indexCadastroCliente", {retorno: null, valores: {nome: req.body.nome, email: req.body.email, senha: req.body.senha}, listaErros:null});
+        }else {
+            console.log(listaErros);
+            return res.render("pages/indexCadastroCliente", {retorno: null, valores: {email: req.body.email, senha: req.body.senha}, listaErros:listaErros});
+        }
+    }
+);
+
+router.get("/cadastronutri", function (req, res) {
+    res.render('pages/indexCadastrarNutri', { retorno: null, valores: {nome:"", telefone:"", email:"", senha:"", area:"", crn:""}, listaErros: null});
+});
+
+router.post(
+    "/cadastrarnutri", 
+    
+    body("nome").isLength({min:2}).withMessage("O nome deve conter 2 ou mais caracteres!"),
+    body("telefone").isMobilePhone().withMessage("Insira um número de telefone válido!"),
+    body("email").isEmail().withMessage("Insira um Email válido!"),
+    body("senha").isLength({min:5}).withMessage("A senha deve conter 5 ou mais caracteres!"),
+    body("area").isLength({min:2}).isAlpha().withMessage("Insira uma área da nutrição válida!"),
+    body("crn").isLength({min:5}).withMessage("Insira um CRN válido!"),
+    
+    function (req, res) {
+        const listaErros = validationResult(req);
+        if (listaErros.isEmpty()) {
+            return res.render("pages/indexCadastrarNutri", {retorno: null, valores: {nome: req.body.nome, telefone: req.body.telefone, email: req.body.email, senha: req.body.senha, area: req.body.area, crn: req.body.crn}, listaErros:null});
+        }else {
+            console.log(listaErros);
+            return res.render("pages/indexCadastrarNutri", {retorno: null, valores: {nome: req.body.nome, telefone: req.body.telefone, email: req.body.email, senha: req.body.senha, area: req.body.area, crn: req.body.crn}, listaErros:listaErros});
+        }
+    }
+);
 
 
 router.get("/", function (req, res) {
     res.render('pages/indexHome')
 });
 
-router.get("/login", function (req, res) {
-    res.render('pages/indexLogin')
-});
-
 router.get("/cadastro", function (req, res) {
     res.render('pages/indexCadastrar')
-});
-
-router.get("/cadastronutri", function (req, res) {
-    res.render('pages/indexCadastrarNutri')
-});
-
-router.get("/cadastrocliente", function (req, res) {
-    res.render('pages/indexCadastroCliente')
 });
 
 router.get("/entrar", function (req, res) {
