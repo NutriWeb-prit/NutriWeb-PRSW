76ea89162e5f4fe7baee7b0daacb50305f56ccb0
felpsss-leudb <feesouza101@gmail.com>
2025-06-03 18:25:27 -0300

    melhorias no mobile + mudança de menu mobile

--- app/public/js/indexMenu.js
+++ app/public/js/indexMenu.js
@@ -2,15 +2,17 @@ document.addEventListener('DOMContentLoaded', function() {
     const menuIcon = document.querySelector('.menu-icon');
     const mobileMenu = document.querySelector('.mobile-menu');
 
-    menuIcon.addEventListener('click', function() {
-        mobileMenu.classList.toggle('hidden');
+    if (menuIcon && mobileMenu) {
+        menuIcon.addEventListener('click', function() {
+            mobileMenu.classList.toggle('hidden');
 
-        if (menuIcon.textContent === 'menu') {
-            menuIcon.textContent = 'close';
-        } else {
-            menuIcon.textContent = 'menu';
-        }
-    });
+            if (menuIcon.textContent === 'menu') {
+                menuIcon.textContent = 'close';
+            } else {
+                menuIcon.textContent = 'menu';
+            }
+        });
+    }
 });
 
 document.addEventListener('DOMContentLoaded', function() {
